919cc07e697d436da7a7fb6399014beda087ee29
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-06 09:34:51 +0000

    Fix the ShopFinderApplicationTests class
    I configured the @SpringBootTest annotation so the test locates the configuration attached to the ShopFinderApplication class

--- src/test/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/ShopFinderApplicationTests.java
+++ src/test/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/ShopFinderApplicationTests.java
@@ -1,12 +1,13 @@
 package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder;
 
+import com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.application.ShopFinderApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ShopFinderApplication.class)
 public class ShopFinderApplicationTests {
 
 	@Test
