862e1af67d82689d2665cde75e80803122b91f4a
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-06 06:37:48 +0000

    Create the backend application with start.spring.io
    I used the following informations to generate the app:
    # bootVersion=1.5.10
    # dependencies=devtool,web
    # language=java
    # type=gradle-project
    # packageName=com.hidden_founders.jobs.software_engineer_java.coding_challenge
    # artifactId=shop-finder

---
+++ src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/ShopFinderApplication.java
@@ -0,0 +1,12 @@
+package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopFinderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/ShopFinderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopFinderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
