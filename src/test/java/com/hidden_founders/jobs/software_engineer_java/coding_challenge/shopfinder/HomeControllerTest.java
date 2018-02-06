189d743979edb931edbc6ef4e75f1f8ec19d2688
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-06 09:31:15 +0000

    Create a unit test for the HomeController class
    I created the test to assert that the web controller returns the right text

---
+++ src/test/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.application.ShopFinderApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ShopFinderApplication.class)
+@AutoConfigureMockMvc
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getIndex() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World!")));
+    }
+}
