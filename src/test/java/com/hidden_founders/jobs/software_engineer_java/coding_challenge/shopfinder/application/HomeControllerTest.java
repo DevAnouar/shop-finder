144adf4179e3a253ac70d69b3530e34b6ea09878
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-06 10:57:23 +0000

    Create the package "application" under test
    I moved the unit tests for the application layer components into the newly created "application" package and deleted the pointer to the ShopFinderApplication class in the @SpringBootTest annotation because it's no longer needed.

--- src/test/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/application/HomeControllerTest.java
+++ src/test/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/application/HomeControllerTest.java
@@ -1,10 +1,9 @@
-package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder;
+package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.application;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.application.ShopFinderApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ShopFinderApplication.class)
+@SpringBootTest()
 @AutoConfigureMockMvc
 public class HomeControllerTest {
 
--- src/test/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/application/ShopFinderApplicationTests.java
+++ src/test/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/application/ShopFinderApplicationTests.java
@@ -1,13 +1,12 @@
-package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder;
+package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.application;
 
-import com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.application.ShopFinderApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ShopFinderApplication.class)
+@SpringBootTest()
 public class ShopFinderApplicationTests {
 
 	@Test
