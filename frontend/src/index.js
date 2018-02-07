5a9b360dacceca59efcaaafdccc855f7c6b9b0dc
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-07 01:15:41 +0000

    Enable hot module reloading
    The frontend should now be reloaded any time I change and save a javascript or css file, without the browser having to refresh the page.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+if (module.hot) {
+    module.hot.accept()
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
