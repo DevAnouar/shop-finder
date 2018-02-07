82fb4b8c429b2a3219a9b923ddb0aa927f8e668f
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-07 02:29:54 +0000

    Move HomeController to the "api" package.
    The "api" package is a subsytem of the application layer

--- src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/application/api/HomeController.java
+++ src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/application/api/HomeController.java
@@ -1,4 +1,4 @@
-package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.application;
+package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
