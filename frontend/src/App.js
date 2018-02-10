c7c5f19d181647a2c8c508d1502858bc6e87c1f3
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-10 07:41:08 +0000

    Use react-geolocated to display the location of the user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {geolocated} from 'react-geolocated';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,22 +8,30 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
+    /*this.state = {
       hello: "Hello React!"
-    }
+    }*/
   }
 
   componentDidMount() {
-    fetch("/hello")
+    /*fetch("/hello")
       .then(function (response) {
         return response.text();
       })
       .then((text) => {
         this.setState({hello: text})
-      });
+      });*/
   }
 
   render() {
+    var locationParagraph = !this.props.isGeolocationAvailable
+      ? <p>Your browser does not support Geolocation</p>
+      : !this.props.isGeolocationEnabled
+        ? <p>Geolocation is not enabled</p>
+        : this.props.coords
+          ? <p>{this.props.coords.longitude}, {this.props.coords.latitude}</p>
+          : <p>Getting the location data&hellip;</p>;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -32,10 +41,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>{this.state.hello}</p>
+        {locationParagraph}
       </div>
     );
   }
 }
 
-export default App;
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(App);
