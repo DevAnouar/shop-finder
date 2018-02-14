41cac42febe0e5c5d3ab26900787b718b744dd34
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-14 19:50:54 +0000

    Create facade to geolocation submodule

---
+++ backend/src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/domain/geolocation/GeographicalCircleArea.java
@@ -0,0 +1,35 @@
+package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.domain.geolocation;
+
+import com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.domain.model.Location;
+import com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.domain.model.Shop;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class GeographicalCircleArea {
+    private Location center;
+    private double radius;
+
+    private IShopsProviderAdapter shopsProviderAdapter;
+
+    GeographicalCircleArea(double centerLatitude, double centerLongitude, double radius) {
+        this.center = makeCenter(centerLatitude, centerLongitude);
+        this.radius = radius;
+    }
+
+    List<Shop> getShopsWithin() {
+        return shopsProviderAdapter.findShopsWithin(this);
+    }
+
+    public Location getCenter() {
+        return center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    private Location makeCenter(double latitude, double longitude) {
+        return new Location(latitude, longitude);
+    }
+}
--- backend/src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/domain/geolocation/IShopsProviderAdapter.java
+++ backend/src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/domain/geolocation/IShopsProviderAdapter.java
@@ -1,6 +1,7 @@
 package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.domain.geolocation;
 
 import com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.domain.model.Shop;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
