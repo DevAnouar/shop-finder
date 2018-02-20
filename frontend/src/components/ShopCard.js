e9da05a4c493b571fd9d5c70283cb9bfcb6b1e42
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-20 01:58:42 +0000

    Add ShopCard stateless functional Component

---
+++ frontend/src/components/ShopCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, Image, Button } from "semantic-ui-react";
+
+const ShopCard = ({ name, picture }) => (
+  <Card raised color="orange">
+    <Card.Content>
+      <Card.Header>{name}</Card.Header>
+    </Card.Content>
+    <Card.Content textAlign="center">
+      <Image src={picture} fluid />
+    </Card.Content>
+    <Card.Content extra>
+      <Button.Group widths="2">
+        <Button basic color="red">
+          Dislike
+        </Button>
+        <Button basic color="green">
+          Like
+        </Button>
+      </Button.Group>
+    </Card.Content>
+  </Card>
+);
+
+export default ShopCard;
