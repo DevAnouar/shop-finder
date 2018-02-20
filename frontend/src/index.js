2744ea94280ae4a7df6548aa0d455d0f3fafd18d
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-20 01:51:54 +0000

    Include The Semantic UI minified CSS file in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
 
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept()
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
