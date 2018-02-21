9367dedf863c8ecec41a55d1895712cc890b6f81
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-21 10:49:55 +0000

    Handle Item click ui

--- frontend/src/components/HomePageMenu.js
+++ frontend/src/components/HomePageMenu.js
@@ -4,14 +4,22 @@ import { Menu } from 'semantic-ui-react'
 class HomePageMenu extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      activeItem: ''
+    }
   }
 
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    let { activeItem } = this.state
+
     return (
       <Menu fixed='top' size='massive' color='teal' secondary pointing borderless>
         <Menu.Menu position='right'>
-          <Menu.Item link active name='Log In' />
-          <Menu.Item link name='Sign Up' />
+          <Menu.Item name='Log In' active={activeItem === 'Log In'} onClick={this.handleItemClick} />
+          <Menu.Item name='Sign Up' active={activeItem === 'Sign Up'} onClick={this.handleItemClick} />
         </Menu.Menu>
       </Menu>
     )
