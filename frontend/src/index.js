39000747fe6c2269511e16ea3e9a418e476ececf
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-23 13:30:04 +0000

    Wrap up the entire React application with Provider to provide the Redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,18 @@ import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import NearbyShopsPage from "./containers/NearbyShopsPage";
 import {registerObserver} from "react-perf-devtool";
+import {Provider} from "react-redux";
+import store from "./store";
 
 if (module.hot) {
   module.hot.accept()
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerObserver();
 registerServiceWorker();
