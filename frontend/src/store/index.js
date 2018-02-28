e43253f0a356562b09e331ade4e6b74be5ddec2e
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-02-28 17:39:52 +0000

    Configure the store for redux-first-router

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,17 @@
-import {combineReducers, createStore} from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import reducers from "../reducers";
 import history from '../history'
+import { connectRoutes } from 'redux-first-router'
+import routesMap from '../routes'
+
+const { reducer, middleware, enhancer } = connectRoutes(history, routesMap)
 
 const store = createStore(
   combineReducers({
-    ...reducers
-  })
+    ...reducers,
+    location: reducer
+  }),
+  compose(enhancer, applyMiddleware(middleware))
 )
 
 export default store
