fa2daec655da6b74ea28abb51ad36007a8e4aaff
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-03-02 21:24:24 +0000

    Edit Main to use react-universal-component

--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -1,8 +1,23 @@
 import React from 'react'
-import Home from "./Home";
-import NearbyShops from "./NearbyShops";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
+import { Loader } from "semantic-ui-react";
+import universal from 'react-universal-component'
 
-const Main = () => (null)
+const UniversalComponent = universal(props => import(`./${props.page}`), {
+  minDelay: 1000,
+  chunkName: props => props.page,
+  loading: () => <Loader active />
+})
+
+const mapStateToProps = ({ page, ...state }) => ({
+  page,
+  isLoading: isLoading(state)
+})
+
+const ConnectedMain = ({ page, isLoading }) => (
+  <UniversalComponent page={page} isLoading={isLoading} />
+)
+
+const Main = connect(mapStateToProps)(ConnectedMain)
 
 export default Main
