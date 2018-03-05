428adf5b22a0dc2733fb4d9d2ff84bd537fd8000
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-03-05 11:21:59 +0000

    Modify MongoConfig to fit latest AbstractMongoConfiguration

--- backend/src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/infrastructure/persistence/MongoConfig.java
+++ backend/src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/infrastructure/persistence/MongoConfig.java
@@ -1,18 +1,10 @@
 package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.infrastructure.persistence;
 
-import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-import org.springframework.data.mongodb.core.convert.DbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.core.convert.*;
 
 @Configuration
 class MongoConfig extends AbstractMongoConfiguration {
@@ -23,16 +15,20 @@ class MongoConfig extends AbstractMongoConfiguration {
     }
 
     @Override
-    public Mongo mongo() throws Exception {
+    public MongoClient mongoClient() {
         return new MongoClient("127.0.0.1", 27017);
     }
 
     @Bean
+    @Override
     public MappingMongoConverter mappingMongoConverter() throws Exception {
-        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory());
-        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext());
-        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        MappingMongoConverter mmc = super.mappingMongoConverter();
+        mmc.setTypeMapper(customTypeMapper());
+        return mmc;
+    }
 
-        return converter;
+    @Bean
+    public MongoTypeMapper customTypeMapper() {
+        return new DefaultMongoTypeMapper(null);
     }
 }
