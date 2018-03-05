ea3de08ebf592f4c7b5fba1dbf725a3097025147
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-03-05 09:57:03 +0000

    Remove _class from inserted user documents into MongoDB

--- backend/src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/infrastructure/persistence/MongoConfig.java
+++ backend/src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/infrastructure/persistence/MongoConfig.java
@@ -2,11 +2,21 @@ package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfin
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
 class MongoConfig extends AbstractMongoConfiguration {
+
     @Override
     protected String getDatabaseName() {
         return "shop_finder";
@@ -15,5 +25,14 @@ class MongoConfig extends AbstractMongoConfiguration {
     @Override
     public Mongo mongo() throws Exception {
         return new MongoClient("127.0.0.1", 27017);
+    }
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter() throws Exception {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory());
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext());
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return converter;
     }
 }
