ec60542b62720d8ce178d902feba98c76466b9c2
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-03-05 13:21:50 +0000

    Add authentication Facade

---
+++ backend/src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/application/authentication/AuthenticationHandler.java
@@ -0,0 +1,19 @@
+package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.application.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationHandler {
+
+    @Autowired
+    UserDetailsManager userDetailsManager;
+
+    public void signUp(String email, String password) {
+        UserDetails user = User.builder().username(email).password(password).build();
+        userDetailsManager.createUser(user);
+    }
+}
