18f6bf9764f55c62b6a3c7d23d70834c23c56775
Anouar Bellakha <anouar.bellakha@gmail.com>
2018-03-05 12:27:31 +0000

    Refactor to use built-in Spring Security UserDetails interface

--- backend/src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/infrastructure/persistence/MongoFacade.java
+++ backend/src/main/java/com/hidden_founders/jobs/software_engineer_java/coding_challenge/shopfinder/infrastructure/persistence/MongoFacade.java
@@ -1,11 +1,11 @@
 package com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.infrastructure.persistence;
 
-import com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.application.authentication.User;
 import com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.domain.model.Location;
 import com.hidden_founders.jobs.software_engineer_java.coding_challenge.shopfinder.domain.model.Shop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.geo.Circle;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -31,8 +31,8 @@ public class MongoFacade {
         return shops;
     }
 
-    public boolean save(User user) {
-        UserEntity userEntity = new UserEntity(user.getEmail(), user.getPassword());
+    public boolean save(UserDetails user) {
+        UserEntity userEntity = new UserEntity(user.getUsername(), user.getPassword());
         try {
             usersRepository.save(userEntity);
         } catch (Exception exception) {
